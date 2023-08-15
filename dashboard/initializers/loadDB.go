9bd5d6a6674ef5792e54f3ac6e2057567d1a63b0
rohansingh9001 <rohansingh9001@gmail.com>
2023-08-15 12:13:19 +0530

    Dockerized dashboard app and docker-compose in progress

--- dashboard/initializers/loadDB.go
+++ dashboard/initializers/loadDB.go
@@ -2,6 +2,8 @@ package initializers
 
 import (
 	"os"
+	"fmt"
+	"strconv"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,8 +13,10 @@ var DB *gorm.DB
 
 func ConnectToDb() {
 	var err error
-	dsn := os.Getenv("DB")
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	port, _ := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"), port, os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+	DB, err = gorm.Open(postgres.Open(psqlconn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to DB")
 	}
