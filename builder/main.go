55a0e0724dc4247665c0fc3df37c0e5081cda0fa
kunxl-gg <tiwari.25@iitj.ac.in>
2023-09-25 18:07:15 +0530

    added more endpoints

--- builder/main.go
+++ builder/main.go
@@ -1,12 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rohansingh9001/Neura-Launch-Dashboard/builder/controllers"
+)
 
 func main() {
 	r := gin.Default()
 
 	// Routes
-	r.GET("/verifyCheckSum", nil)
+	r.GET("/verifyCheckSum", controllers.VerifyCheckSumControllers)
+	r.GET("/downloadFiles", nil)
+	r.GET("/buildImage", nil)
 
 	// Starting the server
 	r.Run(":8080")
