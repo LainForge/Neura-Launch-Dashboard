f74dd4f6c3c3ea718a8b1a5ec21d23b25d3bc746
TarunAdobe <ttomar@adobe.com>
2023-10-11 13:54:07 +0530

    feat: added route to upload files to s3 bucket

---
+++ dashboard/controllers/filesController.go
@@ -0,0 +1,76 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+
+	"github.com/aws/aws-sdk-go/aws/credentials"
+)
+
+const (
+	AWS_S3_REGION = "ap-southeast-1"
+	AWS_S3_BUCKET = "neura-launch-zips"
+)
+
+
+func connectAWS() *session.Session {
+
+	fmt.Println("Connecting to AWS", os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"))
+
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(AWS_S3_REGION),Credentials: credentials.NewStaticCredentials(
+		os.Getenv("AWS_ACCESS_KEY_ID"),
+		os.Getenv("AWS_SECRET_ACCESS_KEY"),
+		""),})
+	if err != nil {
+		panic(err)
+	}
+	return sess
+}
+
+
+func UploadFile(c *gin.Context) {
+
+	var sess = connectAWS()
+
+	// Parse the multipart form, with max file size 100MB
+	err := c.Request.ParseMultipartForm(100 << 20)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse multipart form"})
+		return
+	}
+
+	// Get a file from the form input name "file"
+	file, header, err := c.Request.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to retrieve the file from the form"})
+		return
+	}
+	defer file.Close()
+
+	filename := header.Filename
+
+	// Upload the file to S3.
+	uploader := s3manager.NewUploader(sess)
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(AWS_S3_BUCKET), // Bucket
+		Key:    aws.String(filename),      // Name of the file to be saved
+		Body:   file,                      // File
+	})
+
+	fmt.Println("Error........: ", err)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to upload the file"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "File uploaded successfully", "bucket": AWS_S3_BUCKET})
+}
