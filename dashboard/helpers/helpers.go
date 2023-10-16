08a4c3394c85f28dd47c44ed4ec8862824f6479f
TarunAdobe <ttomar@adobe.com>
2023-10-16 12:04:39 +0530

    feat: added project routes

--- dashboard/helpers/helpers.go
+++ dashboard/helpers/helpers.go
@@ -1,6 +1,11 @@
 package helpers
 
-// CheckError handler 
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+// CheckError handler
 func CheckError(err error) {
 	if err != nil {
 		panic(err)
@@ -12,4 +17,19 @@ func CheckErrorWithMessage(err error, message string) {
 	if err != nil {
 		panic(message)
 	}
+}
+
+// GenerateRandomToken generates a random token of length
+func GenerateProjectToken(length int) (string, error) {
+	if length%2 != 0 {
+		return "", nil
+	}
+
+	bytes := make([]byte, length/2)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(bytes), nil
 }
