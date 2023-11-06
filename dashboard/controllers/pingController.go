9006a9f198810c7ecb8506c7f148a0ac4da9c1e4
Ishwarya-Selvaraj <77732470+Ishwarya-Selvaraj@users.noreply.github.com>
2023-11-06 11:31:04 +0530

    Update pingController.go

--- dashboard/controllers/pingController.go
+++ dashboard/controllers/pingController.go
@@ -1,20 +1,20 @@
 package controllers
 
 import (
-	"net/http"
+    "net/http"
 
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
 )
 
 func Ping(ctx *gin.Context) {
-	ctx.JSON(
-		http.StatusOK,
-		gin.H{
-			"message": "Hello World!",
-		})
+    ctx.JSON(
+        http.StatusOK,
+        gin.H{
+            "message": "Hello World!",
+        })
 }
 
 func Hello(ctx *gin.Context) {
-	ctx.Header("Content-Type", "text/html; charset=utf-8")
-	ctx.String(http.StatusOK, "<h1>Hello World! Kunal is a fucking genius</h1>")
+    ctx.Header("Content-Type", "text/html; charset=utf-8")
+    ctx.String(http.StatusOK, "<h1>Hello World! Kunal is a fucking genius</h1>")
 }
