b1d9a82ecec4895800f1ae8b92e98cf32bf187c3
Ishwarya-Selvaraj <77732470+Ishwarya-Selvaraj@users.noreply.github.com>
2023-11-06 11:32:10 +0530

    Update tokenController.go

--- dashboard/controllers/tokenController.go
+++ dashboard/controllers/tokenController.go
@@ -1,26 +1,26 @@
 package controllers
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/LainForge/Neura-Launch-Dashboard/dashboard/helpers"
-	"golang.org/x/crypto/bcrypt"
+    "github.com/gin-gonic/gin"
+    "github.com/LainForge/Neura-Launch-Dashboard/dashboard/helpers"
+    "golang.org/x/crypto/bcrypt"
 )
 
 func TokenController(ctx *gin.Context) {
 
-	var body struct {
-		Email string
-	}
+    var body struct {
+        Email string
+    }
 
-	err := ctx.Bind(&body)
-	helpers.CheckErrorWithMessage(err, "Failed to read body")
+    err := ctx.Bind(&body)
+    helpers.CheckErrorWithMessage(err, "Failed to read body")
 
-	hash, err := bcrypt.GenerateFromPassword([]byte(body.Email), 10)
-	helpers.CheckError(err)
+    hash, err := bcrypt.GenerateFromPassword([]byte(body.Email), 10)
+    helpers.CheckError(err)
 
-	ctx.JSON(
-		200,
-		gin.H{"token": string(hash)},
-	)
+    ctx.JSON(
+        200,
+        gin.H{"token": string(hash)},
+    )
 
 }
