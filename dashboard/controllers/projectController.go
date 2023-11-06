9e90cf7547b9178d2d17b9840118bf70c5cb501d
Ishwarya-Selvaraj <77732470+Ishwarya-Selvaraj@users.noreply.github.com>
2023-11-06 11:31:40 +0530

    Update projectController.go

--- dashboard/controllers/projectController.go
+++ dashboard/controllers/projectController.go
@@ -1,124 +1,124 @@
 package controllers
 
 import (
-	"net/http"
+    "net/http"
 
-	"github.com/LainForge/Neura-Launch-Dashboard/dashboard/helpers"
-	"github.com/LainForge/Neura-Launch-Dashboard/dashboard/initializers"
-	"github.com/LainForge/Neura-Launch-Dashboard/dashboard/models"
-	"github.com/gin-gonic/gin"
+    "github.com/LainForge/Neura-Launch-Dashboard/dashboard/helpers"
+    "github.com/LainForge/Neura-Launch-Dashboard/dashboard/initializers"
+    "github.com/LainForge/Neura-Launch-Dashboard/dashboard/models"
+    "github.com/gin-gonic/gin"
 )
 
 func CreateNewProject(ctx *gin.Context) {
 
-	user, exists := ctx.Get("user")
+    user, exists := ctx.Get("user")
 
-	if !exists {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch user"})
-		return
-	}
+    if !exists {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch user"})
+        return
+    }
 
-	// Get the project name
-	var body struct {
-		Name string
-	}
+    // Get the project name
+    var body struct {
+        Name string
+    }
 
-	err := ctx.Bind(&body)
+    err := ctx.Bind(&body)
 
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Failed to read body"})
-		return
-	}
+    if err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": "Failed to read body"})
+        return
+    }
 
-	// if the project name is taken
-	var project_exists models.Project
+    // if the project name is taken
+    var project_exists models.Project
 
-	result := initializers.DB.Where("name = ?", body.Name).First(&project_exists)
+    result := initializers.DB.Where("name = ?", body.Name).First(&project_exists)
 
-	if result.Error == nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Project name already taken"})
-		return
-	}
+    if result.Error == nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": "Project name already taken"})
+        return
+    }
 
-	// generate a new token
-	token, err := helpers.GenerateProjectToken(32)
+    // generate a new token
+    token, err := helpers.GenerateProjectToken(32)
 
-	// Create the project
-	project := models.Project{Name: body.Name, Token: token, UserID: user.(models.User).ID}
+    // Create the project
+    project := models.Project{Name: body.Name, Token: token, UserID: user.(models.User).ID}
 
-	result = initializers.DB.Create(&project)
+    result = initializers.DB.Create(&project)
 
-	if result.Error != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create project"})
-		return
-	}
+    if result.Error != nil {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create project"})
+        return
+    }
 
-	// Respond
-	ctx.JSON(http.StatusOK, gin.H{"message": "Project Created Successfully", "token": token})
+    // Respond
+    ctx.JSON(http.StatusOK, gin.H{"message": "Project Created Successfully", "token": token})
 }
 
 // Get all projects for a user
 func GetProjects(ctx *gin.Context) {
 
-	user, exists := ctx.Get("user")
+    user, exists := ctx.Get("user")
 
-	if !exists {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch user"})
-		return
-	}
+    if !exists {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch user"})
+        return
+    }
 
-	var projects []models.Project
+    var projects []models.Project
 
-	result := initializers.DB.Where("user_id = ?", user.(models.User).ID).Find(&projects)
+    result := initializers.DB.Where("user_id = ?", user.(models.User).ID).Find(&projects)
 
-	if result.Error != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch projects"})
-		return
-	}
+    if result.Error != nil {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch projects"})
+        return
+    }
 
-	ctx.JSON(http.StatusOK, gin.H{"projects": projects})
+    ctx.JSON(http.StatusOK, gin.H{"projects": projects})
 }
 
 // Get a project by token
 func GetProject(ctx *gin.Context) {
 
-	user, exists := ctx.Get("user")
+    user, exists := ctx.Get("user")
 
-	if !exists {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch user"})
-		return
-	}
+    if !exists {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch user"})
+        return
+    }
 
-	var project models.Project
+    var project models.Project
 
-	result := initializers.DB.Where("token = ? AND user_id = ?", ctx.Param("token"), user.(models.User).ID).First(&project)
+    result := initializers.DB.Where("token = ? AND user_id = ?", ctx.Param("token"), user.(models.User).ID).First(&project)
 
-	if result.Error != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch project"})
-		return
-	}
+    if result.Error != nil {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch project"})
+        return
+    }
 
-	ctx.JSON(http.StatusOK, gin.H{"project": project})
+    ctx.JSON(http.StatusOK, gin.H{"project": project})
 }
 
 // Delete a project by token
 func DeleteProject(ctx *gin.Context) {
 
-	user, exists := ctx.Get("user")
+    user, exists := ctx.Get("user")
 
-	if !exists {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch user"})
-		return
-	}
+    if !exists {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch user"})
+        return
+    }
 
-	var project models.Project
+    var project models.Project
 
-	result := initializers.DB.Where("token = ? AND user_id = ?", ctx.Param("token"), user.(models.User).ID).Delete(&project)
+    result := initializers.DB.Where("token = ? AND user_id = ?", ctx.Param("token"), user.(models.User).ID).Delete(&project)
 
-	if result.Error != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete project"})
-		return
-	}
+    if result.Error != nil {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete project"})
+        return
+    }
 
-	ctx.JSON(http.StatusOK, gin.H{"message": "Project deleted successfully"})
+    ctx.JSON(http.StatusOK, gin.H{"message": "Project deleted successfully"})
 }
