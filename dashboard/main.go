8058cf0854d6ca2bbab1b48c062df5c64d41615a
Ishwarya-Selvaraj <77732470+Ishwarya-Selvaraj@users.noreply.github.com>
2023-11-06 11:40:11 +0530

    Update main.go

--- dashboard/main.go
+++ dashboard/main.go
@@ -1,61 +1,61 @@
 package main
 
 import (
-	"github.com/LainForge/Neura-Launch-Dashboard/dashboard/controllers"
-	"github.com/LainForge/Neura-Launch-Dashboard/dashboard/initializers"
-	"github.com/LainForge/Neura-Launch-Dashboard/dashboard/middlewares"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	log "github.com/sirupsen/logrus"
+    "github.com/LainForge/Neura-Launch-Dashboard/dashboard/controllers"
+    "github.com/LainForge/Neura-Launch-Dashboard/dashboard/initializers"
+    "github.com/LainForge/Neura-Launch-Dashboard/dashboard/middlewares"
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
+    log "github.com/sirupsen/logrus"
 )
 
 func init() {
-	initializers.LoanEnvVariables()
-	initializers.ConnectToDb()
-	initializers.SyncDatabase()
-	initializers.Logging()
+    initializers.LoanEnvVariables()
+    initializers.ConnectToDb()
+    initializers.SyncDatabase()
+    initializers.Logging()
 }
 
 func main() {
 
-	// Initializing the gin router/engine
-	r := gin.New()
+    // Initializing the gin router/engine
+    r := gin.New()
 
-	// Setup middleware
-	r.Use(gin.Recovery())
-	r.Use(middlewares.LoggingMiddleware())
+    // Setup middleware
+    r.Use(gin.Recovery())
+    r.Use(middlewares.LoggingMiddleware())
 
-	// Allowing Cross Origin Requests
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:8080", "http://localhost:5173"},
-		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		AllowCredentials: true,
-	}))
+    // Allowing Cross Origin Requests
+    r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:3000", "http://localhost:8080", "http://localhost:5173"},
+        AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        AllowCredentials: true,
+    }))
 
-	// Auth URLs
-	r.POST("/signup", controllers.Signup)
-	r.POST("/login", controllers.Login)
-	r.GET("/validate", middlewares.RequireAuth, controllers.Validate)
+    // Auth URLs
+    r.POST("/signup", controllers.Signup)
+    r.POST("/login", controllers.Login)
+    r.GET("/validate", middlewares.RequireAuth, controllers.Validate)
 
-	// Generate Token
-	r.POST("/token", middlewares.RequireAuth, controllers.TokenController)
+    // Generate Token
+    r.POST("/token", middlewares.RequireAuth, controllers.TokenController)
 
-	// Upload Code to S3
-	r.POST("/upload", controllers.UploadFile)
+    // Upload Code to S3
+    r.POST("/upload", controllers.UploadFile)
 
-	// Project URLs
-	r.POST("/project/new", middlewares.RequireAuth, controllers.CreateNewProject)
-	r.GET("/projects", middlewares.RequireAuth, controllers.GetProjects)
-	r.GET("/project/:token", middlewares.RequireAuth, controllers.GetProject)
-	r.DELETE("/project/:token", middlewares.RequireAuth, controllers.DeleteProject)
+    // Project URLs
+    r.POST("/project/new", middlewares.RequireAuth, controllers.CreateNewProject)
+    r.GET("/projects", middlewares.RequireAuth, controllers.GetProjects)
+    r.GET("/project/:token", middlewares.RequireAuth, controllers.GetProject)
+    r.DELETE("/project/:token", middlewares.RequireAuth, controllers.DeleteProject)
 
-	// Ping endpoint
-	r.GET("/ping", controllers.Ping)
-	r.GET("", controllers.Hello)
+    // Ping endpoint
+    r.GET("/ping", controllers.Ping)
+    r.GET("", controllers.Hello)
 
-	// Starting the Server
-	log.Info("Server started at port 8080")
-	r.Run()
+    // Starting the Server
+    log.Info("Server started at port 8080")
+    r.Run()
 
 }
